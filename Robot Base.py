19b4c9ef704af31fbfa0edfce39962527c550e07
Rocket Fish <spraints@gmail.com>
2024-09-28 17:21:44 -0400

    Sync with new version of mind-meld and new sync script

--- Robot Base.py
+++ Robot Base.py
@@ -7,25 +7,31 @@ def main():
     # goal is to be able to write code like this:
     robot = Kraken()
     robot.reset_angle()
- 
+
+    # robot.turn_left(90)
+    # wait_for_seconds(1)
+    # robot.show_state()
+    # return
+
     # test program: trace letter H
     robot.show_state()
     robot.drive_forward(80)
     robot.show_state()
-    #robot.drive_backward(40)
+    robot.drive_backward(40)
     robot.turn_right(90)
     robot.drive_forward(40)
     robot.turn_left(90)
     robot.drive_forward(40)
-    #robot.drive_backward(80)
+    robot.drive_backward(80)
+    wait_for_seconds(1)
     robot.show_state()
 
 
 class Kraken:
     def __init__(self):
         self.wheel_diameter = 8.7 # cm
-        left_motor_port = 'A'
-        right_motor_port = 'B'
+        left_motor_port = 'B'
+        right_motor_port = 'F'
         self.left_motor = Motor(left_motor_port)
         self.right_motor = Motor(right_motor_port)
         self.motor_pair = MotorPair(left_motor_port, right_motor_port)
@@ -47,19 +53,31 @@ class Kraken:
             self .motor_pair.start(correction*10,40)
         self.motor_pair.stop()
 
+    def drive_backward(self, distance):
+        # convert distance (centimeters) to degrees
+        distance_in_degrees = distance * (360.0 / (self.wheel_diameter * math.pi))
+        start_position = self.right_motor.get_degrees_counted()
+        goal_position = start_position - distance_in_degrees
+        while self.right_motor.get_degrees_counted() > goal_position:
+            correction = self.angle_goal - self.motion_sensor.get_yaw_angle()
+            self .motor_pair.start(-correction*10,-40)
+        self.motor_pair.stop()
+
     
     def turn_left(self, degrees):
         self.angle_goal = self.angle_goal - degrees
+        turn_goal = self.angle_goal + 20
         self.motor_pair.start_tank(-50, 50)
-        while self.motion_sensor.get_yaw_angle()>self.angle_goal:
+        while self.motion_sensor.get_yaw_angle()>turn_goal:
             # wait
             True
         self.motor_pair.stop()
 
     def turn_right(self, degrees):
         self.angle_goal = self.angle_goal + degrees
+        turn_goal = self.angle_goal - 20
         self.motor_pair.start_tank(50, -50)
-        while self.motion_sensor.get_yaw_angle()<self.angle_goal:
+        while self.motion_sensor.get_yaw_angle()<turn_goal:
             # wait
             True
         self.motor_pair.stop()
